85da00acaf5424a9421db2e7b4bcd9d52f06431c
Richard Schoen <richard@richardschoen.net>
2022-01-17 10:11:15 -0600

    Create pyrichconsolemarkup1.py
    Create colorized Python command line apps

---
+++ ibmi/pyrichconsolemarkup1.py
@@ -0,0 +1,11 @@
+#------------------------------------------------------------------------
+# Create colorized console markup for your IBM i and Linux Python apps
+# https://rich.readthedocs.io/en/latest/markup.html
+#
+# Pip packages:
+# pip3 install rich
+#------------------------------------------------------------------------
+
+#Sample colorized text
+from rich import print
+print("[bold red]alert![/bold red] [blue]Something happened on the IBM i system[/blue]")
