56f64bec6f53185bed4aecc9466bcfdc2626dfe4
Richard Schoen <richard@richardschoen.net>
2023-08-02 18:52:12 -0500

    Update pysftprmtcmd.py

--- ibmi/pysftprmtcmd.py
+++ ibmi/pysftprmtcmd.py
@@ -10,7 +10,7 @@
 #
 # Pip packages needed:
 # pip3 install pysftp
-# pip3 install pysftp-extension
+# pip3 install pysftp-extension (may not be needed)
 #
 # Parameters
 # --sftphost/-host=SFTP Host
